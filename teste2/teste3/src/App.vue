192df6fcacaf73129b6e0b091b8d03080a3c1d0f
alujuan <99190123+alujuan@users.noreply.github.com>
2022-09-21 09:19:18 -0300

    Add files via upload

---
+++ teste2/teste3/src/App.vue
@@ -0,0 +1,36 @@
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      cep: "",
+      endereco: {},
+    };
+  },
+  methods: {
+    async buscar() {
+      const url = `https://viacep.com.br/ws/${this.cep}/json/`;
+      const res = await axios.get(url);
+      this.endereco = res.data;
+    },
+  },
+};
+</script>
+
+<template>
+  <main>
+    <h1>Buscar CEP</h1>
+    <form @submit.prevent="buscar">
+      <input type="text" v-model="cep" />
+      <button type="submit">Buscar</button>
+    </form>
+    <form class="end">
+      <label for="rua">Rua</label>
+      <input type="text" v-model="endereco.logradouro" />
+      <label for="rua">Número</label>
+      <input type="text" v-model="endereco.numero" />
+      <label for="rua">Bairro</label>
+      <input type="text" v-model="endereco.bairro" />
+    </form>
+  </main>
+</template>
